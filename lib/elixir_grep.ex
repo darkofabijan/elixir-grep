1873e469ff228d095b9621644afc91a67b915fca
Darko Fabijan <darko@renderedtext.com>
2014-12-19 01:45:58 +0100

    parallel impl

--- lib/elixir_grep.ex
+++ lib/elixir_grep.ex
@@ -1,27 +1,35 @@
 defmodule ElixirGrep do
 
   def run do
-    query = "Found"
-
     Path.wildcard("./**/*")
       |> only_files
-      |> find_in_files
+      |> start_searches(user_query)
+      |> wait_for_results
+  end
+
+  def user_query do
+    #List.first(System.argv)
+    "tasks"
   end
 
   def only_files(paths) do
     Enum.filter(paths, &File.regular?/1)
   end
 
-  def find_in_files(paths) do
-    Enum.each(paths, &find_in_file/1)
+  def start_searches(paths, query) do
+    Enum.map(paths, &(Task.async(ElixirGrep, :find_in_file, [&1, query])))
   end
 
-  def find_in_file(path) do
+  def wait_for_results(tasks) do
+    Enum.each(tasks, &Task.await/1)
+  end
+
+  def find_in_file(path, query) do
     File.open(path, [:read], fn(file) ->
       lines = IO.read(file, :all)
 
-      if String.contains?(lines, "paths") do
-        IO.puts "Found 'module' in #{path}"
+      if String.contains?(lines, query) do
+        IO.puts "Found '#{query}' in #{path}"
       end
     end)
   end
