d455ba678b7c389399153eed0f4bbdd91d399318
Darko Fabijan <darko@renderedtext.com>
2014-12-19 01:15:31 +0100

    seq impl

--- lib/elixir_grep.ex
+++ lib/elixir_grep.ex
@@ -1,2 +1,29 @@
 defmodule ElixirGrep do
+
+  def run do
+    query = "Found"
+
+    Path.wildcard("./**/*")
+      |> only_files
+      |> find_in_files
+  end
+
+  def only_files(paths) do
+    Enum.filter(paths, &File.regular?/1)
+  end
+
+  def find_in_files(paths) do
+    Enum.each(paths, &find_in_file/1)
+  end
+
+  def find_in_file(path) do
+    File.open(path, [:read], fn(file) ->
+      lines = IO.read(file, :all)
+
+      if String.contains?(lines, "paths") do
+        IO.puts "Found 'module' in #{path}"
+      end
+    end)
+  end
+
 end
